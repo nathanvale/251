9d65316dc877ecf1c9f66348d918b3cd1c1a6385
Nathan Vale <hi@nathanvale.com>
2020-03-04 09:53:33 +1100

    fix(theme): add px suffix to MUI line heights

--- packages/ods-themes/src/_private/mui-themes/originRetail/typography.ts
+++ packages/ods-themes/src/_private/mui-themes/originRetail/typography.ts
@@ -11,39 +11,39 @@ export const typography = {
   text: {
     xxxsmall: {
       fontSize: 12,
-      lineHeight: 20,
+      lineHeight: "20px",
     },
     xxsmall: {
       fontSize: 14,
-      lineHeight: 24,
+      lineHeight: "24px",
     },
     xsmall: {
       fontSize: 16,
-      lineHeight: 24,
+      lineHeight: "24px",
     },
     small: {
       fontSize: 18,
-      lineHeight: 24,
+      lineHeight: "24px",
     },
     medium: {
       fontSize: 20,
-      lineHeight: 28,
+      lineHeight: "28px",
     },
     large: {
       fontSize: 24,
-      lineHeight: 28,
+      lineHeight: "28px",
     },
     xlarge: {
       fontSize: 32,
-      lineHeight: 40,
+      lineHeight: "40px",
     },
     xxlarge: {
       fontSize: 36,
-      lineHeight: 40,
+      lineHeight: "40px",
     },
     xxxlarge: {
       fontSize: 56,
-      lineHeight: 64,
+      lineHeight: "64px",
     },
   },
 };
