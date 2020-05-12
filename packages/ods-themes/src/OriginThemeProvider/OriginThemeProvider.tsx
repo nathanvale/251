304a9127cc952419db94c32467c751a350cca187
Ali Motevallian <ali.motevallian@originenergy.com.au>
2020-05-12 21:38:35 +1000

    fix(originthemeprovider): remove loading fonts
    Fonts now load relatively to acoid CORS issues

--- packages/ods-themes/src/OriginThemeProvider/OriginThemeProvider.tsx
+++ packages/ods-themes/src/OriginThemeProvider/OriginThemeProvider.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ThemeProvider, injectGlobal } from "styled-components";
+import { ThemeProvider /*injectGlobal*/ } from "styled-components";
 import {
   ThemeProvider as MUIThemeProvider,
   Theme as MuiTheme,
@@ -14,56 +14,6 @@ export interface OriginThemeProviderProps {
   muiTheme?: MuiTheme;
   children: React.ReactNode;
 }
-
-// eslint-disable-next-line babel/no-unused-expressions
-injectGlobal`
-      html {
-        background-color: #f1f1f1;
-        height:100%;
-      }
-
-      body {
-        margin: 0;
-        font-family: gordita, sans-serif;
-        height: 100%;
-      }
-
-      body > div {
-        height: 100%;
-      }
-
-      @font-face {
-        font-family: 'gordita';
-        src: url(https://origin-fonts.s3-ap-southeast-2.amazonaws.com/Origin_Gordita-Light.woff2)
-          format('woff');
-        font-weight: 200;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: 'gordita';
-        src: url(https://origin-fonts.s3-ap-southeast-2.amazonaws.com/Origin_Gordita-Regular.woff2)
-          format('woff');
-        font-weight: 500;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: 'gordita';
-        src: url(https://origin-fonts.s3-ap-southeast-2.amazonaws.com/Origin_Gordita-Medium.woff2)
-          format('woff');
-        font-weight: 600;
-        font-style: normal;
-      }
-
-      @font-face {
-        font-family: 'gordita';
-        src: url(https://origin-fonts.s3-ap-southeast-2.amazonaws.com/Origin_Gordita-Bold.woff2)
-          format('woff');
-        font-weight: 900;
-        font-style: normal;
-      }
-`;
 
 export const OriginThemeProvider = ({
   children,
