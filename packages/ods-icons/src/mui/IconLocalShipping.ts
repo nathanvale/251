07a93f7c86a18ce5b5918a985c1540ed9cf91343
Ali Motevallian <ali.motevallian@originenergy.com.au>
2020-09-01 14:56:20 +1000

    feat(ods-icons): add icons for help-support

---
+++ packages/ods-icons/src/mui/IconLocalShipping.ts
@@ -0,0 +1,3 @@
+import LocalShipping from "@material-ui/icons/LocalShipping";
+import { createMUISvgIcon } from "../_private/createMUISvgIcon";
+export default createMUISvgIcon("LocalShipping", LocalShipping);
---
+++ packages/ods-icons/src/mui/IconTextsms.ts
@@ -0,0 +1,3 @@
+import Textsms from "@material-ui/icons/Textsms";
+import { createMUISvgIcon } from "../_private/createMUISvgIcon";
+export default createMUISvgIcon("Textsms", Textsms);
